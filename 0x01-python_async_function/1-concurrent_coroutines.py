d2bafe7033ad01239ee609e4a9f2ab8c6ad57967
root <ezakariaa>
2024-10-14 21:53:04 +0000

    0x01. Python - Async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-""" The basics of async """
+""" async proyect """
 
 import asyncio
 from typing import List
+
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Spawn wait_random n times with the specified max_delay.
-    Returns:
-        List[float]: List of delay values.
-    """
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+    """ Return a list of delays in asc order """
+    delays: List[float] = []
 
+    for _ in range(n):
+        delays.append(wait_random(max_delay))
+
+    return [await process for process in asyncio.as_completed(delays)]
